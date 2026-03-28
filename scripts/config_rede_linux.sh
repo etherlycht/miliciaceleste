71b106b2baa90e9bdf98515f9a95623fec518446
miliciaceleste <etherlycht@hotmail.com>
2026-03-28 18:03:05 -0300

    Update network memory buffer settings in config_rede_linux.sh
    Added default memory buffer settings for network configuration.

--- scripts/config_rede_linux.sh
+++ scripts/config_rede_linux.sh
@@ -19,7 +19,10 @@ sudo sysctl -w net.ipv4.tcp_slow_start_after_idle=0
 # Buffers de Memória de Rede
 sudo sysctl -w net.core.rmem_max=16777216
 sudo sysctl -w net.core.wmem_max=16777216
+sudo sysctl -w net.core.rmem_default= 16777216
+sudo sysctl -w net.core.wmem_default= 16777216
 sudo sysctl -w net.core.netdev_max_backlog=5000
+sudo sysctl -w net.ipv4.udp_mem="768000 1024000 1536000"
 sudo sysctl -w net.core.dev_weight=64
 
 # --- Otimizando Hardware de Rede ---
@@ -34,7 +37,7 @@ else
 fi
 
 # --- Processo do Jogo ---
-echo "--- Aguardando o Elite Dangerous (Steam/Proton) ---"
+echo "--- Aguardando o Elite Dangerous (Wine) ---"
 # Loop que espera o jogo abrir para capturar o PID
 PID=""
 while [ -z "$PID" ]; do
